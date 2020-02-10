db7b1a28aef0afab877b674022c7e48915569403
Michael B <consulting@belfrage.net>
2020-02-10 10:56:16 +0100

    Rearchitecture for v1.0.0
    1. Make `Emlx` inherit from `email.message.Message`.
    2. Support reads from file-like objects.
    3. Better documentation and examples.
    4. More intuitive API.
    5. Tests.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="emlx",
-    version="0.1.0",
+    version="1.0.0",
     url="https://github.com/mikez/emlx",
     project_urls={
         "Code": "https://github.com/mikez/emlx",
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,4 @@
+import io
 import os.path
 
 import pytest
@@ -6,14 +7,23 @@ import emlx
 
 
 TEST_FILE = "test.emlx"
-# flags = 8623489089
-# flags = (
+TEST_BYTECOUNT = 545
+TEST_MESSAGE_ID = "<7A129E26-2C1F-4517-B6B5-39460ED50E12@example.com>"
+TEST_PAYLOAD_LENGTH = 73
+# raw_flags = 8623489089
+# = (
 #     (1 << 0)  # read
 #     + (1 << 6)  # draft
 #     + (1 << 25)  # is not junk
 #     + (1 << 33)  # (something undocumented Apple adds)
 # )
-TEST_FLAGS = {"read": True, "draft": True, "is_not_junk": True}
+TEST_PLIST = {
+    "conversation-id": 123456,
+    "date-last-viewed": 1581111111,
+    "date-received": 1581000000,
+    "flags": {"read": True, "draft": True, "is_not_junk": True},
+    "remote-id": "789",
+}
 
 
 @pytest.fixture
@@ -23,27 +33,74 @@ def message_filepath(request):
 
 
 @pytest.fixture
-def message(message_filepath, request):
+def message_content(message_filepath):
+    with open(message_filepath, "rb") as filebuffer:
+        return filebuffer.read()
+
+
+@pytest.fixture
+def message(message_filepath):
     return emlx.read(message_filepath)
 
 
 def test_bytecount(message):
-    assert message.bytecount == 545
+    assert message.bytecount == TEST_BYTECOUNT
 
 
-def test_data(message):
-    assert (
-        message.data["Message-Id"]
-        == "<7A129E26-2C1F-4517-B6B5-39460ED50E12@example.com>"
-    )
+def test_message_mime_content(message):
+    assert message.headers["Message-Id"] == TEST_MESSAGE_ID
+    assert message.id == TEST_MESSAGE_ID
+    assert message.url == "message:" + TEST_MESSAGE_ID
+    assert len(message.get_payload()) == TEST_PAYLOAD_LENGTH
 
 
-def test_flags(message):
-    assert message.plist["flags"] == TEST_FLAGS
+def test_plist_and_flags(message):
+    assert message.plist == TEST_PLIST
+    assert message.flags == TEST_PLIST["flags"]
 
 
-def test_message_plist_only(message_filepath, request):
+def test_message_buffered_as_bytesio(message_content):
+    filebuffer = io.BytesIO(message_content)
+    message = emlx.read(filebuffer)
+    assert message.bytecount == TEST_BYTECOUNT
+    assert message.id == TEST_MESSAGE_ID
+    assert len(message.get_payload()) == TEST_PAYLOAD_LENGTH
+    assert message.plist["flags"] == TEST_PLIST["flags"]
+
+
+def test_message_buffered_via_open(message_filepath):
+    with open(message_filepath, "rb") as filebuffer:
+        message = emlx.read(filebuffer)
+    assert message.bytecount == TEST_BYTECOUNT
+    assert message.id == TEST_MESSAGE_ID
+    assert len(message.get_payload()) == TEST_PAYLOAD_LENGTH
+    assert message.plist["flags"] == TEST_PLIST["flags"]
+
+
+def test_message_plist_only(message_filepath):
     message = emlx.read(message_filepath, plist_only=True)
-    assert message.bytecount == 545
-    assert message.data is None
-    assert message.plist["flags"] == TEST_FLAGS
+    assert message.bytecount == TEST_BYTECOUNT
+    assert message.url is None
+    assert message.get_payload() is None
+    assert message.plist == TEST_PLIST
+    assert message.flags == TEST_PLIST["flags"]
+
+
+def test_message_plist_only_as_bytesio(message_content):
+    filebuffer = io.BytesIO(message_content)
+    message = emlx.read(filebuffer, plist_only=True)
+    assert message.bytecount == TEST_BYTECOUNT
+    assert message.url is None
+    assert message.get_payload() is None
+    assert message.plist == TEST_PLIST
+    assert message.flags == TEST_PLIST["flags"]
+
+
+def test_message_plist_only_via_open(message_filepath):
+    with open(message_filepath, "rb") as filebuffer:
+        message = emlx.read(filebuffer, plist_only=True)
+    assert message.bytecount == TEST_BYTECOUNT
+    assert message.url is None
+    assert message.get_payload() is None
+    assert message.plist == TEST_PLIST
+    assert message.flags == TEST_PLIST["flags"]
